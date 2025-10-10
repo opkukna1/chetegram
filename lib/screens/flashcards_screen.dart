b4b635a373ef52644c37461a68087c4f4fa79120
opkukna1 <Opsiddh41@gmail.com>
2025-10-10 13:37:08 +0530

    Create flashcards_screen.dart

---
+++ lib/screens/flashcards_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class FlashcardsScreen extends StatelessWidget {
+  const FlashcardsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Flashcards'),
+      ),
+      body: const Center(
+        child: Text('Flashcards Screen'),
+      ),
+    );
+  }
+}
