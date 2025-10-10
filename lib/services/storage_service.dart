80b2afdd3852cd653d3549e134b0c037f6ddbaa8
opkukna1 <Opsiddh41@gmail.com>
2025-10-11 00:50:01 +0530

    Update storage_service.dart

--- lib/services/storage_service.dart
+++ lib/services/storage_service.dart
@@ -1,18 +1,12 @@
 import 'dart:io';
 import 'package:firebase_storage/firebase_storage.dart';
-import 'package:image_picker/image_picker.dart';
 
 class StorageService {
   final FirebaseStorage _storage = FirebaseStorage.instance;
-  final ImagePicker _picker = ImagePicker();
 
-  // इमेज चुनें और अपलोड करें
-  Future<String?> pickAndUploadImage(String path) async {
+  // यह फंक्शन एक File object और path लेता है और इमेज अपलोड करके उसका URL देता है
+  Future<String?> uploadImage(String path, File file) async {
     try {
-      final XFile? image = await _picker.pickImage(source: ImageSource.gallery);
-      if (image == null) return null;
-
-      File file = File(image.path);
       TaskSnapshot snapshot = await _storage.ref().child(path).putFile(file);
       return await snapshot.ref.getDownloadURL();
     } catch (e) {
