8bbd987ad82b607619a6a6b18afd07e2dd618f81
opkukna1 <Opsiddh41@gmail.com>
2025-10-11 10:48:33 +0530

    Update storage_service.dart

--- lib/services/storage_service.dart
+++ lib/services/storage_service.dart
@@ -4,7 +4,6 @@ import 'package:firebase_storage/firebase_storage.dart';
 class StorageService {
   final FirebaseStorage _storage = FirebaseStorage.instance;
 
-  // यह फंक्शन एक File object और path लेता है और इमेज अपलोड करके उसका URL देता है
   Future<String?> uploadImage(String path, File file) async {
     try {
       TaskSnapshot snapshot = await _storage.ref().child(path).putFile(file);
