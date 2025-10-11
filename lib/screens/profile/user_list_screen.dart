4b0922478a7af39b07855490478f7004ad7a050e
opkukna1 <Opsiddh41@gmail.com>
2025-10-11 10:06:23 +0530

    Create user_list_screen.dart

---
+++ lib/screens/profile/user_list_screen.dart
@@ -0,0 +1,75 @@
+import 'package:chetegram/models/user_model.dart';
+import 'package:chetegram/services/firestore_service.dart';
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+enum UserListType { followers, following }
+
+class UserListScreen extends StatefulWidget {
+  final String userId;
+  final UserListType listType;
+
+  const UserListScreen({
+    super.key,
+    required this.userId,
+    required this.listType,
+  });
+
+  @override
+  State<UserListScreen> createState() => _UserListScreenState();
+}
+
+class _UserListScreenState extends State<UserListScreen> {
+  final FirestoreService _firestoreService = FirestoreService();
+  late Future<List<UserModel>> _usersFuture;
+
+  @override
+  void initState() {
+    super.initState();
+    if (widget.listType == UserListType.followers) {
+      _usersFuture = _firestoreService.getFollowers(widget.userId);
+    } else {
+      _usersFuture = _firestoreService.getFollowing(widget.userId);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.listType == UserListType.followers ? 'Followers' : 'Following'),
+      ),
+      body: FutureBuilder<List<UserModel>>(
+        future: _usersFuture,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          }
+          if (!snapshot.hasData || snapshot.data!.isEmpty) {
+            return Center(child: Text('No users found in this list.'));
+          }
+
+          final users = snapshot.data!;
+          return ListView.builder(
+            itemCount: users.length,
+            itemBuilder: (context, index) {
+              final user = users[index];
+              return ListTile(
+                leading: CircleAvatar(
+                  backgroundImage: user.profilePicUrl.isNotEmpty ? NetworkImage(user.profilePicUrl) : null,
+                  child: user.profilePicUrl.isEmpty ? const Icon(Icons.person) : null,
+                ),
+                title: Text(user.name),
+                subtitle: Text('@${user.email.split('@')[0]}'),
+                onTap: () {
+                  // उस यूज़र की प्रोफाइल पर जाएं
+                  context.push('/profile/${user.uid}');
+                },
+              );
+            },
+          );
+        },
+      ),
+    );
+  }
+}
